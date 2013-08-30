2271a8e1d180f33c286b240b11ac8b44ac83a53d
Jeremy Casey <jeremy8883@gmail.com>
2013-08-30 16:12:10 -0400

    Initial commit with ability to draw, clear and download

---
+++ Live PaintR/PaintHub.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using System.Drawing;
+using Newtonsoft.Json;
+
+namespace Live_PaintR
+{
+    public class PaintHub : Hub
+    {
+        private static readonly ConcurrentStack<Stroke> _strokes = new ConcurrentStack<Stroke>();
+
+        public void SendDrawing(Stroke stroke)
+        {
+            _strokes.Push(stroke);
+            Clients.AllExcept(Context.ConnectionId).broadcastDrawing(stroke);
+        }
+
+        public void ClearDrawing()
+        {
+            _strokes.Clear();
+            Clients.All.broadcastClear();
+        }
+
+        public IEnumerable<Stroke> RequestFullDrawing()
+        {
+            return _strokes.ToArray();
+        }
+    }
+    public class Stroke
+    {
+        [JsonProperty("color")]
+        public string Color { get; set; }
+        [JsonProperty("points")]
+        public int[,] Points { get; set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+Live PaintR
+===========
+by Jeremy Casey, 2013-08-28
+
+A simple drawing canvas web app, made using .Net and SignalR. Anything you draw on the canvas is broadcast to all connected users in real time. It's just a quick practice app I made while learning SignalR at the Toronto .Net Hackers Meet Up.
+
+NOTE: Pen colour selection is only supported in chrome.
+
+![Screenshot](/readme/Screenshot.png "Screenshot")
